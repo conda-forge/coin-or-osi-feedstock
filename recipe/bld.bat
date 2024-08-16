ce3d1283c72d738d4290e3eab28c54ab218f40f9
julianuspfeuffer <julianus.pfeuffer@pfizer.com>
2024-08-16 15:04:21 +0200

    try quoting

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,19 @@
 copy "%RECIPE_DIR%\build.sh" .
+set PREFIX_WIN=%PREFIX%
 set PREFIX=%PREFIX:\=/%
+set LIBRARY_PREFIX_WIN=%LIBRARY_PREFIX%
 set LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%
-set LIBRARY_PREFIX_EXPANDED=%LIBRARY_PREFIX:%PREFIX%=%PREFIX%%
+
+rem Temporarily store the expanded value of PREFIX
+set TEMP_PREFIX=%PREFIX%
+rem Expand LIBRARY_PREFIX by replacing %PREFIX% with the actual value of TEMP_PREFIX
+call set LIBRARY_PREFIX_EXPANDED=%%LIBRARY_PREFIX:%PREFIX%=%TEMP_PREFIX%%%
+
+rem Temporarily store the expanded value of PREFIX
+set TEMP_PREFIX_WIN=%PREFIX_WIN%
+rem Expand LIBRARY_PREFIX by replacing %PREFIX% with the actual value of TEMP_PREFIX
+call set LIBRARY_PREFIX_WIN_EXPANDED=%%LIBRARY_PREFIX_WIN:%PREFIX%=%TEMP_PREFIX_WIN%%%
+
 set SRC_DIR=%SRC_DIR:\=/%
 set MSYSTEM=MINGW%ARCH%
 set MSYS2_PATH_TYPE=inherit
