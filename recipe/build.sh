4eced9336e2e87e7ad4c0511d9276a9b54024e5c
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-08-11 13:36:10 +0200

    Fix Windows platform detection for flang migration

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ else
     USE_PREFIX=$PREFIX
 fi
 
-if [[ "${target_platform}" == win-* ]]; then
+if [[ -n ${LIBRARY_PREFIX+x} ]]; then
   COINUTILS_LIB=( --with-coinutils-lib='${LIBRARY_PREFIX}/lib/mkl_rt.lib ${LIBRARY_PREFIX}/lib/libCoinUtils.lib' )
   COINUTILS_INC=( --with-coinutils-incdir='${LIBRARY_PREFIX_COIN}' )
   EXTRA_FLAGS=( --enable-msvc=MD ) 
@@ -16,7 +16,7 @@ else
   # Get an updated config.sub and config.guess (for mac arm and lnx aarch64)
   cp $BUILD_PREFIX/share/gnuconfig/config.* ./Osi 
   cp $BUILD_PREFIX/share/gnuconfig/config.* .
-  COINUTLS_LIB=()
+  COINUTILS_LIB=()
   COINUTILS_INC=()
   EXTRA_FLAGS=()
 fi
